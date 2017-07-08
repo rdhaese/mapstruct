07f9a098aff786167b0eb64d8c0e8dadcc8270f2
Robin D'Haese <r.dhaese92@gmail.com>
2017-07-08 20:19:02 +0200

    Added ComponentModel class with constants for convinience. Set spring as default component model to use on generation. (Generated classes will be annotated with @Component)

---
+++ src/main/java/com/rdhaese/commons/mapstruct/constants/ComponentModel.java
@@ -0,0 +1,13 @@
+package com.rdhaese.commons.mapstruct.constants;
+
+/**
+ * Created on 8/07/2017.
+ *
+ * @author Robin D'Haese
+ */
+public final class ComponentModel {
+
+    public static final String DEFAULT = "default";
+    public static final String SPRING = "spring";
+    public static final String CDI = "cdi";
+}
